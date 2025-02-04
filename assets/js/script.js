dfed6cc0f832d14c39bf3417f932d75b7272b65e
azeemhdr <comazeem@gmail.com>
2025-02-04 16:57:25 +0530

    instructors section complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,35 @@ videobtn.addEventListener("click", function () {
 // =========================slider============================
 $(document).ready(function () {
   $(".testimonial-slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 2000,
-	arrow:true,
+	arrows:true,
     prevArrow:
       '<button type="button" class="slick-prev">←</button>',
     nextArrow:
       '<button type="button" class="slick-next">→</button>',
   });
 });
+
+$(document).ready(function(){
+  $(".instructor-wrapper").slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows:true,
+    dots:true,
+    // prevArrow:
+    //   '<button type="button" class="slick-prev">←</button>',
+    // nextArrow:
+    //   '<button type="button" class="slick-next">→</button>',
+  })
+})
+
+
